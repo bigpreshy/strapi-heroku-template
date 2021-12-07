9522a380fdb0b5f2e49fd800aa0c21a09577cc4c
Preshy <lukeprecious@gmail.com>
2021-12-07 18:24:40 +0100

    Added production database

--- config/database.js
+++ config/database.js
@@ -1,19 +1,11 @@
+const path = require('path');
 
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
-      },
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
